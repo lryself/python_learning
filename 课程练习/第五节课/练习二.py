52fd0e5dc82163d44d4b150968bb16e46ea102be
lryself <lnolvwe@163.com>
2020-03-04 09:55:15 +0800

    提交练习

---
+++ 课程练习/第五节课/练习二.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+'''
+@File : 练习二.py
+@Time : 2020/03/04 08:30:19
+@Author : lryself 
+@Version : 1.0
+@Contact : lryself@163.com
+'''
+
+# here put the import lib
+'''题目：提示输入需要购买的苹果的重量(斤),然后提示输入每斤的价格,请计算应支付的总价,并打印提示输出;
+'''
+jing=int(input("请输入需要购买的苹果的重量(斤)"))
+price=int(input("请输入每斤的价格"))
+print(jing*price)
