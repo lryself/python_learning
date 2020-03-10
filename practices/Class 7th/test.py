2b4fb7b96202e8d27c09eb2647bbd7016a5e1c73
lryself <lnolvwe@163.com>
2020-03-10 18:41:26 +0800

    lnolvwe@163.com

---
+++ practices/Class 7th/test.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+'''
+@File : test.py
+@Time : 2020/03/10 18:40:01
+@Author : lryself 
+@Version : 1.0
+@Contact : lnolvwe@163.com
+题目：
+'''
+
+# here put the import lib
+print("hallo world")
