657e1beea32ee244a25ddf8ec3be0e40045f6b76
lryself <lnolvwe@163.com>
2020-03-18 10:50:12 +0800

    3月18日练习

---
+++ practices/Class 9th/s1_os_path.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+'''
+@File : s1_do_os_path.py
+@Time : 2020/03/18 08:31:55
+@Author : lryself 
+@Version : 1.0
+@Contact : lnolvwe@163.com
+题目：
+'''
+
+# here put the import lib
+import os
+
+path1=os.path.abspath("F:\编程\GitHub\python_learning\practices\Class 9th\os_practice")
+print( os.path.basename(path1) )   # 返回文件名
+print( os.path.dirname(path1) )    # 返回目录路径
+print( os.path.split(path1) )      # 分割文件名与路径
+print( os.path.join('path1','test','run1.txt') )  # 将目录和文件名合成一个路径
